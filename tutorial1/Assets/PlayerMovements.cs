4a75b671146f4c328c5e861eb7f6c38e978581d6
200202iqbal <200202yr@yse-c.net>
2021-04-12 00:54:00 +0900

    tutorial01
    tutorial01

---
+++ tutorial1/Assets/PlayerMovements.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public Rigidbody rb;
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
