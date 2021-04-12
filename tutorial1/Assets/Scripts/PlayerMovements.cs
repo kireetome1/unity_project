c38f96f5739ef0f66e13f4f5db9f5f50c7c115ae
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-04-12 13:55:33 +0900

    unity

--- tutorial1/Assets/Scripts/PlayerMovements.cs
+++ tutorial1/Assets/Scripts/PlayerMovements.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce = 1500f;
+    public float sidewaysForce = 500f;
     
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
